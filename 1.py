e2fe0122318b183e9f2f5f9cb6d9d4a428447812
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-08-14 12:23:49 +0200

    add hand detector

--- 1.py
+++ 1.py
@@ -11,9 +11,16 @@ cap = cv2.VideoCapture(0)
 cap.set(3,800)
 cap.set(4,600)
 
+detector = HandDetector(detectionCon=0.7 , maxHands=1)
+
 while True:
     ok , img = cap.read()
 
     img = cv2.flip(img,1)
+
+    hands , img = detector.findHands(img , flipType=False)
+
     cv2.imshow("snack game [python ai]" , img)
+
+    key = cv2.waitKey(1)
 

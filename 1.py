8d2a52252f4c5953c78d963dde7092e3d24bd992
IMILY ABDERRAZZK <imily2024@gmail.com>
2025-08-14 11:44:41 +0200

    snack ai

---
+++ 1.py
@@ -0,0 +1,15 @@
+import cv2
+import cvzone
+import random
+import math
+
+from cvzone.HandTrackingModule import HandDetector
+
+
+cap = cv2.VideoCapture(0)
+
+cap.set(3,800)
+cap.set(4,600)
+
+while True:
+    ok , img = 
